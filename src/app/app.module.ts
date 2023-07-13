e0180f085c324e9d19db2102403cee4b8d5d02b3
MykhailoKhr <khrystenko.m.y@gmail.com>
2023-07-13 18:14:50 +0300

    FIX | fix module issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageContainerComponent } from './modules/main-page/container/main-page-container/main-page-container.component';
 import {HeaderModule} from "./core/components/header/header.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageContainerComponent
   ],
   imports: [
     BrowserModule,
